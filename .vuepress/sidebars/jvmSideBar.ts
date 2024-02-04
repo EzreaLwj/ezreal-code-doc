cfa6fd5e353f272b428e8a82e5e7d825628f7b2a
ezreal <956371950@qq.com>
2024-02-05 01:13:23 +0800

    feat: 完善垃圾收集模块文章

--- .vuepress/sidebars/jvmSideBar.ts
+++ .vuepress/sidebars/jvmSideBar.ts
@@ -6,6 +6,7 @@ export default [
         children: [
             '/JVM/自动内存管理/Java内存管理与溢出异常',
             '/JVM/自动内存管理/垃圾收集器与内存分配策略',
+            '/JVM/自动内存管理/HotSpot的算法细节实现',
         ],
     },
 ]
