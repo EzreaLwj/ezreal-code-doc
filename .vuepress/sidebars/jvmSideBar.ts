43dca8b64fb948ecffe717129f1befe8d3fcd895
ezreal <956371950@qq.com>
2024-02-14 20:01:10 +0800

    feat: 添加JVM相关文章

--- .vuepress/sidebars/jvmSideBar.ts
+++ .vuepress/sidebars/jvmSideBar.ts
@@ -1,7 +1,7 @@
 export default [
     "",
     {
-        title: "内存管理",
+        title: "自动内存管理",
         collapsable: true,
         children: [
             '/JVM/自动内存管理/Java内存管理与溢出异常',
@@ -9,5 +9,23 @@ export default [
             '/JVM/自动内存管理/HotSpot的算法细节实现',
             '/JVM/自动内存管理/虚拟机监控工具',
         ],
+    },
+    {
+        title: "虚拟机执行子系统",
+        collapsable: true,
+        children: [
+            '/JVM/虚拟机执行子系统/类文件结构',
+            '/JVM/虚拟机执行子系统/虚拟机类加载机制',
+        ],
+
+    },
+    {
+        title: "高效并发",
+        collapsable: true,
+        children: [
+            '/JVM/高效并发/Java内存模型与线程',
+            '/JVM/高效并发/线程安全与锁优化',
+        ],
+
     },
 ]
